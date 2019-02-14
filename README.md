a0af70369f36412265100c335518cd4069c2d389
lzfxxx <lzfxxx@gmail.com>
2019-02-14 14:08:07 +0800

    general cookiecutter

---
+++ README.md
@@ -0,0 +1,27 @@
+# MO mo-cookiecutter-python
+
+A [cookiecutter](https://github.com/audreyr/cookiecutter) template for creating
+a MO module in Python. (See also
+[mo-cookiecutter-python](https://github.com/xxxx/xxxx))
+
+## Use the template to create package
+
+Install cookiecutter.
+
+```
+pip install cookiecutter
+```
+
+Use cookiecutter to generate a package, following the prompts to fill in the name and authorship of your new JupyterLab extension.
+
+```
+cookiecutter https://github.com/xxxx/xxxx
+```
+
+## A simple example
+
+The ``src/`` directory of your new extension includes a very simple example of a working extension, written in TypeScript. Use this example as a guide to build your own extension.
+
+## Package name
+
+We suggest that simple extension names start with `mo_` and use underscores if needed to improve readability, such as `mo_mymodule`.
---
+++ {{cookiecutter.module_name}}/main.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+# sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+class {{cookiecutter.module_name}}(object):
+
+    def __init__(self, conf={}):
+        """
+
+        :param input:
+        """
+        self.checkpoint_dir = "./results"
+        pass
+
+    def train(self, conf={}):
+        """
+
+        :param conf:
+        :return:
+        """
+        param1 = conf['param1']  # value_type: str # description: some description
+        param2 = conf['param2']  # value_type: str # description: some description
+        # add your code
+        return {'ret1': 'cat', 'ret2': 'dog'}
+
+    def run(self, conf={}):
+        """
+
+        :param conf:
+        :return:
+        """
+        param1 = conf['param1']  # value_type: str # description: some description
+        param2 = conf['param2']  # value_type: str # description: some description
+        # add your code
+        return {'ret1': 'cat', 'ret2': 'dog'}
+
+    def load_model(self, file="./results/{{cookiecutter.module_name}}.pkl"):
+        """
+
+        :param conf:
+        :return:
+        """
+        pass
+
+
+## Note: Uncomment this block before creating a crowdsourcing task
+# if __name__ == '__main__':
+#     func_name = sys.argv[0]
+#     instance = {{cookiecutter.module_name}}()
+#     getattr(instance, func_name)()
